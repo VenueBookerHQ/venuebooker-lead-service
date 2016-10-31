8ff89ba5997ddd25527b3a002df024b3848f3e6b
Apoclyps <kyle90adam@hotmail.com>
2016-10-31 17:58:32 +0000

    Initial commit of Django REST-api

---
+++ base_site/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+import web_app.views
+
+admin.autodiscover()
+
+# Examples:
+# url(r'^$', 'base_site.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
+urlpatterns = [
+    url(r'^$', web_app.views.index, name='index'),
+    url(r'^web_app/', include('web_app.urls')),
+    url(r'', include('web_app.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
---
+++ web_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from models import Venue
+
+# Create your views here.
+def index(request):
+    venues = Venue.objects.order_by('priority')
+
+    return render(request, 'index.html', {'venues': venues})
+
+
+def venue_list(request):
+    venues = Venue.objects.order_by('priority')
+
+    if len(venues) == 0:
+        venues = {}
+
+    return render(request, 'index.html', {'venues': venues})
+
