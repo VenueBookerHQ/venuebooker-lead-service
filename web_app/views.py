b2ac05d361aa0daefdd8bccf1a649fc9c7aca0d3
Adam <fatal.toxicity@gmail.com>
2016-11-05 17:56:26 +0000

    Fixes to index template, added initial migration file

--- web_app/views.py
+++ web_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from models import Venue
+from web_app.models import Venue
 
-# Create your views here.
 def index(request):
     venues = Venue.objects.order_by('priority')
 
@@ -17,4 +16,3 @@ def venue_list(request):
         venues = {}
 
     return render(request, 'index.html', {'venues': venues})
-
