b33cc29e07f36cf9a4cfb2cbdfcfde3ea4a5c59a
GregorW14 <gregwhyte14@gmail.com>
2017-01-18 20:58:31 +0000

    Initial Commit

---
+++ venuebooker-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gregor/Documents/venuebooker/venuebooker-api-service/venuebooker-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
