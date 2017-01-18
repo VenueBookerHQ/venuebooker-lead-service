a22dff8ccf087f3be4a0e9ad0372ffcf734c77f1
GregorW14 <gregwhyte14@gmail.com>
2017-01-18 22:32:07 +0000

    Initial Commit3

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Venue
+#from .models import Venue
 
-admin.site.register(Venue)
+#admin.site.register(Venue)
--- web_app/views.py
+++ web_app/views.py
@@ -5,16 +5,16 @@ from .models import *
 
 # Create your views here.
 def index(request):
-    venues = Venue.objects.order_by('priority')
+    #venues = Venue.objects.order_by('priority')
 
-    return render(request, 'index.html', {'venues': venues})
+    return render(request, 'index.html', {})
 
 
 def venue_list(request):
-    venues = Venue.objects.order_by('priority')
+    #venues = Venue.objects.order_by('priority')
 
-    if len(venues) == 0:
-        venues = {}
+    #if len(venues) == 0:
+     #   venues = {}
 
-    return render(request, 'index.html', {'venues': venues})
+    return render(request, 'index.html', {})
 
