b7ed04620c7097e485e24cb5d31a2cfaddc19b66
GregorW14 <gregwhyte14@gmail.com>
2017-01-19 15:10:50 +0000

    Added test page

--- web_app/urls.py
+++ web_app/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.venue_list, name='venue_list')
+    url(r'^$', views.venue_list, name='venue_list'),
     url(r'^login$', views.login, name='login')
 ]
