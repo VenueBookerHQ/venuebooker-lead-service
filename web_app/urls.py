9b823fe4813a1b2d6e0d99a12cda332f7dc0cf66
GregorW14 <gregwhyte14@gmail.com>
2017-01-19 15:33:32 +0000

    Minor changes

--- web_app/urls.py
+++ web_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.venue_list, name='venue_list'),
+    url(r'^index$', views.index, name='index'),
     url(r'^login$', views.login, name='login')
 ]
