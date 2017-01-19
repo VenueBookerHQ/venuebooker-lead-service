f46e643abe32969620e6afeceb7648de962261e4
GregorW14 <gregwhyte14@gmail.com>
2017-01-19 15:23:07 +0000

    Added test page

--- web_app/models.py
+++ web_app/models.py
@@ -10,3 +10,13 @@ from django.db import models
 
     #def __str__(self):              # __unicode__ on Python 2
      #   return self.type
+
+#class Venue(models.Model):
+  #  priority = models.IntegerField()
+   # type = models.CharField(max_length=30)
+   # description = models.TextField(max_length=200)
+    #places = models.IntegerField()
+   # image = models.CharField(max_length=100)
+
+    #def __str__(self):              # __unicode__ on Python 2
+     #   return self.type
