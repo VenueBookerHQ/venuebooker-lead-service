5823e1dcdbd190b766a88e754d8d1d1ba1240ba2
GregorW14 <gregwhyte14@gmail.com>
2017-01-31 19:44:08 +0000

    Add image upload fields

--- web_app/models.py
+++ web_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Venue(models.Model):
     type = models.CharField(max_length=30)
     description = models.TextField(max_length=200)
-    image = models.CharField(max_length=100)
+    image = models.FileField()
 
     def __str__(self):              # __unicode__ on Python 2
         return self.type
@@ -13,7 +13,7 @@ class Event_campaign(models.Model):
     type = models.CharField(max_length=30)
     details = models.TextField(max_length=200)
     name = models.TextField(max_length=200)
-    image = models.CharField(max_length=100)
+    image = models.FileField()
     capacity = models.IntegerField()
 
     def __str__(self):              # __unicode__ on Python 2
