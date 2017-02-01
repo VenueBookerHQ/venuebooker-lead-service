cc41159987b49ae20c539955095a795dd6fe73c9
GregorW14 <gregwhyte14@gmail.com>
2017-02-01 02:18:49 +0000

    Bug fix

--- web_app/urls.py
+++ web_app/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^venues/(?P<pk>[0-9]+)/$', views.DetailViewVenue.as_view(), name='venue_detail'),
     url(r'^venues/add/$', views.VenueCreate.as_view(), name='venue-add'),
     url(r'^eventcampaigns/$', views.EventCampaignList.as_view(), name='event_campaign_list'),
-    url(r'^eventcampaigns/(?P<pk>[0-9]+)/$', views.DetailViewEvent, name='event_campaign_detail'),
+    url(r'^eventcampaigns/(?P<pk>[0-9]+)/$', views.DetailViewEvent.as_view(), name='event_campaign_detail'),
     url(r'^eventcampaigns/add/$', views.EventCampaignCreate.as_view(), name='event_campaign-add'),
 ]
