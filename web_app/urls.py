3c9370f9ddc9bf989dd0523f4597487ceb11215e
GregorW14 <gregwhyte14@gmail.com>
2017-02-01 02:09:10 +0000

    Urls fix

--- web_app/urls.py
+++ web_app/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     url(r'^index$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
     url(r'^venues/$', views.VenueList.as_view(), name='venue_list'),
-    url(r'^venues/(?P<pk>[0-9]+)/$', views.DetailViewVenue.as_view(), name='detail'),
+    url(r'^venues/(?P<pk>[0-9]+)/$', views.DetailViewVenue.as_view(), name='venue_detail'),
     url(r'^venues/add/$', views.VenueCreate.as_view(), name='venue-add'),
+    url(r'^eventcampaigns/$', views.EventCampaignList.as_view(), name='event_campaign_list'),
     url(r'^eventcampaigns/(?P<pk>[0-9]+)/$', views.DetailViewEvent, name='event_campaign_detail'),
     url(r'^eventcampaigns/add/$', views.EventCampaignCreate.as_view(), name='event_campaign-add'),
 ]
