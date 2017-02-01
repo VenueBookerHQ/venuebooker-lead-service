5e9b3e5702782808a35b473b0352022a5a007ee4
GregorW14 <gregwhyte14@gmail.com>
2017-02-01 01:08:47 +0000

    url fixes

--- web_app/urls.py
+++ web_app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^index$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
     url(r'^venues/$', views.VenueList.as_view(), name='venue_list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^venues/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^eventcampaigns/$', views.event_campaign_list, name='event_campaign_list'),
 ]
