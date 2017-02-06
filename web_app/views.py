e20257aff4e5942434e3ac30c3c4a3f402ea22f6
GregorW14 <gregwhyte14@gmail.com>
2017-02-06 01:33:42 +0000

    Register added

--- web_app/views.py
+++ web_app/views.py
@@ -1,10 +1,13 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import View
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
 from .models import *
+from .forms import UserForm
 
 # Create your views here.
 def index(request):
@@ -67,3 +70,41 @@ class EventCampaignDelete(DeleteView):
 class VenueDelete(DeleteView):
     model = Venue
     success_url = reverse_lazy('index')
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'web_app/register_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form' : form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+        
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+        
+            if user is not None:
+                
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+
+        return render(request, self.template_name, {'form' : form})
+
+
+
+
+
+
+
+
+
