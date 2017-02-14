5f6b8d38bb1be95df73529f5b29c6ab4969649fd
GregorW14 <gregwhyte14@gmail.com>
2017-02-14 20:30:38 +0000

    User Profile

--- venuebooker-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/gregor/Documents/venuebooker/venuebooker-api-service/venuebooker-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
