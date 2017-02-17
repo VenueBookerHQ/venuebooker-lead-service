de79a998a42f21e27828107a081f87e60d78ba12
GregorW14 <gregwhyte14@gmail.com>
2017-02-17 02:19:21 +0000

    Add OAuth-toolkit urls

--- base_site/urls.py
+++ base_site/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     url(r'^web_app/', include('web_app.urls')),
     url(r'', include('web_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
 
 
