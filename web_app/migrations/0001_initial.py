976ec45299106c58759bf7ba1611e7da821a171b
GregorW14 <gregwhyte14@gmail.com>
2017-03-02 20:59:08 +0000

    Fix

--- web_app/migrations/0001_initial.py
+++ web_app/migrations/0001_initial.py
@@ -183,7 +183,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customuser',
             name='contact',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='web_app.Contact'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, null=True, to='web_app.Contact'),
         ),
         migrations.AddField(
             model_name='customuser',
--- web_app/models.py
+++ web_app/models.py
@@ -121,7 +121,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(_('active'), default=True,
         help_text=_('Designates whether this user should be treated as '
                     'active. Unselect this instead of deleting accounts.'))
-    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True)
     date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
     is_active = models.BooleanField(_('active'), default=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
