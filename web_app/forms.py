b4e7c2c85a56447582d2b78ed620ca2aca8f9726
GregorW14 <gregwhyte14@gmail.com>
2017-03-02 20:00:10 +0000

    Fix

--- web_app/forms.py
+++ web_app/forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+#from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from web_app.models import CustomUser
+#from web_app.models import CustomUser
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput)
@@ -11,30 +11,30 @@ class UserForm(forms.ModelForm):
 		model = User
 		fields = ['username', 'email', 'password']
 
-class CustomUserCreationForm(UserCreationForm):
-    """
-    A form that creates a user, with no privileges, from the given email and
-    password.
-    """
+#class CustomUserCreationForm(UserCreationForm):
+ #   """
+  #  A form that creates a user, with no privileges, from the given email and
+   # password.
+   # """
 
-    def __init__(self, *args, **kargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kargs)
+ #   def __init__(self, *args, **kargs):
+  #      super(CustomUserCreationForm, self).__init__(*args, **kargs)
 
-    class Meta:
-        model = CustomUser
-        fields = ("username",)
+   # class Meta:
+    #    model = CustomUser
+     #   fields = ("username",)
 
 
-class CustomUserChangeForm(UserChangeForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
+#class CustomUserChangeForm(UserChangeForm):
+ #   """A form for updating users. Includes all the fields on
+  #  the user, but replaces the password field with admin's
+   # password hash display field.
+    #"""
 
-    def __init__(self, *args, **kargs):
-        super(CustomUserChangeForm, self).__init__(*args, **kargs)
+    #def __init__(self, *args, **kargs):
+     #   super(CustomUserChangeForm, self).__init__(*args, **kargs)
 
-    class Meta:
-        model = CustomUser
-        fields = ("username",)
+    #class Meta:
+     #   model = CustomUser
+      #  fields = ("username",)
 
