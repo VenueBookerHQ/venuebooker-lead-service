62db4f700a6e78735723daeab8770b964f887ece
GregorW14 <gregwhyte14@gmail.com>
2017-03-02 22:11:01 +0000

    Fix

--- web_app/models.py
+++ web_app/models.py
@@ -122,7 +122,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         help_text=_('Designates whether this user should be treated as '
                     'active. Unselect this instead of deleting accounts.'))
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE, null=True)
-    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    date_joined = models.DateTimeField(_('date joined'), default=datetime.datetime.now)
     is_active = models.BooleanField(_('active'), default=True)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
