344e6e4fea6f905c0830459fd5586a922494be50
GregorW14 <gregwhyte14@gmail.com>
2017-03-02 19:42:55 +0000

    Added New User Model

--- base_site/settings.py
+++ base_site/settings.py
@@ -67,7 +67,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-AUTH_USER_MODEL = 'web_app.CustomUser'
+AUTH_USER_MODEL = 'User'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '745625103135-o9k6ma41dvpdaabqfh0rn1slenuligmp.apps.googleusercontent.com'
