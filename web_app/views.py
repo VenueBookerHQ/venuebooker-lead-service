9b505130e4c8ceb0124cf6c3e422f890efc7a1f3
GregorW14 <gregwhyte14@gmail.com>
2017-03-06 00:59:18 +0000

    Testing auto user assign Enquiry

--- web_app/views.py
+++ web_app/views.py
@@ -96,7 +96,10 @@ class EventCampaignDelete(DeleteView):
 
 class EnquiryCreate(CreateView):
 	model = Enquiry
-	fields = ['message', 'attendeeNum', 'date', 'event_campaign', 'user']
+	fields = ['message', 'attendeeNum', 'date', 'event_campaign']
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(EnquiryCreate, self).form_valid(form)
 
 class RegisterView(View):
     form_class = UserForm
