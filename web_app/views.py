c3111b9eb580aa05578a22475a77e4ca14774711
GregorW14 <gregwhyte14@gmail.com>
2017-04-12 11:56:07 +0100

    User update contact details

--- web_app/views.py
+++ web_app/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404, render_to_response
 from django.contrib.auth import authenticate, login as auth_login, logout
 from .models import *
-from .forms import UserForm, ContactForm, ContactResponseForm, UserFormUpdate, CustomUserChangeForm
+from .forms import UserForm, ContactForm, ContactResponseForm, ProfileForm, CustomUserChangeForm
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 from django.contrib.auth.forms import PasswordChangeForm
