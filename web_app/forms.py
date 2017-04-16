b5d2aad78ea7f529316f3591340c2b1590df7275
GregorW14 <gregwhyte14@gmail.com>
2017-04-16 22:21:54 +0100

    Password confirm

--- web_app/forms.py
+++ web_app/forms.py
@@ -9,22 +9,22 @@ from web_app.models import CustomUser
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput)
-    password_confirm = forms.CharField(widget=forms.PasswordInput)
+	password_confirm = forms.CharField(widget=forms.PasswordInput)
 	
 	class Meta:
 		model = CustomUser
 		fields = ['username', 'avatar', 'password', 'password_confirm']
-    
-    def clean(self):
-        clean_data = super(UserForm, self).clean()
+	
+	def clean(self):
+		clean_data = super(UserForm, self).clean()
 
-        password = clean_data.get('password')
-        password_confirm = clean_data.get('password_confirm')
+		password = clean_data.get('password')
+		password_confirm = clean_data.get('password_confirm')
 
-        if password and password_confirm:
-            if password != password_confirm:
-                raise forms.ValidationError("The passwords do not match.")
-        return clean_data
+		if password and password_confirm:
+			if password != password_confirm:
+				raise forms.ValidationError("The passwords do not match.")
+		return clean_data
 
 class ProfileForm(forms.ModelForm):	
 	class Meta:
@@ -32,66 +32,66 @@ class ProfileForm(forms.ModelForm):
 		fields = ['avatar']
 
 class CustomUserCreationForm(UserCreationForm):
-    """
-    A form that creates a user, with no privileges, from the given email and
-    password.
-    """
+	"""
+	A form that creates a user, with no privileges, from the given email and
+	password.
+	"""
 
-    def __init__(self, *args, **kargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kargs)
+	def __init__(self, *args, **kargs):
+		super(CustomUserCreationForm, self).__init__(*args, **kargs)
 
-    class Meta:
-        model = CustomUser
-        fields = ("username","email")
+	class Meta:
+		model = CustomUser
+		fields = ("username","email")
 
 
 class CustomUserChangeForm(UserChangeForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
+	"""A form for updating users. Includes all the fields on
+	the user, but replaces the password field with admin's
+	password hash display field.
+	"""
 
-    def __init__(self, *args, **kargs):
-        super(CustomUserChangeForm, self).__init__(*args, **kargs)
+	def __init__(self, *args, **kargs):
+		super(CustomUserChangeForm, self).__init__(*args, **kargs)
 
-    class Meta:
-        model = CustomUser
-        fields = ("username","email")
+	class Meta:
+		model = CustomUser
+		fields = ("username","email")
 
 class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['first_name', 'last_name', 'email', 'telephone', 'mobile', 'company']
+	class Meta:
+		model = Contact
+		fields = ['first_name', 'last_name', 'email', 'telephone', 'mobile', 'company']
 
 class ContactResponseForm(forms.ModelForm):
-    class Meta:
-        model = ContactResponse
-        fields = ['name', 'email', 'phone', 'message']
+	class Meta:
+		model = ContactResponse
+		fields = ['name', 'email', 'phone', 'message']
 
 class OrganisationForm(forms.ModelForm):
-    class Meta:
-        model = Organisation
-        fields = ['name', 'image', 'address', 'primary_contact', 'description']
+	class Meta:
+		model = Organisation
+		fields = ['name', 'image', 'address', 'primary_contact', 'description']
 
 class VenueForm(forms.ModelForm):
-    class Meta:
-        model = Venue
-        fields = ['name', 'type', 'image', 'address', 'city', 'country', 'quoteImage', 'facebook_link', 'twitter_link', 'instagram_link', 'description', 'organisation']
+	class Meta:
+		model = Venue
+		fields = ['name', 'type', 'image', 'address', 'city', 'country', 'quoteImage', 'facebook_link', 'twitter_link', 'instagram_link', 'description', 'organisation']
 
 class EventCampaignForm(forms.ModelForm):
-    class Meta:
-        model = Event_campaign
-        fields = ['name', 'type', 'details', 'startTime', 'endTime', 'recurring', 'image', 'capacity', 'cost_per_capacity_unit', 'venue']
+	class Meta:
+		model = Event_campaign
+		fields = ['name', 'type', 'details', 'startTime', 'endTime', 'recurring', 'image', 'capacity', 'cost_per_capacity_unit', 'venue']
 
 class EnquiryForm(forms.ModelForm):
-    class Meta:
-        model = Enquiry
-        fields = ['message', 'attendeeNum', 'date', 'event_campaign', 'user', 'approved']
+	class Meta:
+		model = Enquiry
+		fields = ['message', 'attendeeNum', 'date', 'event_campaign', 'user', 'approved']
 
 class QuoteForm(forms.ModelForm):
-    class Meta:
-        model = Quote
-        fields = ['description', 'cost', 'accepted', 'enquiry']
+	class Meta:
+		model = Quote
+		fields = ['description', 'cost', 'accepted', 'enquiry']
 
 class OrganisationUserForm(forms.ModelForm):
 	class Meta:
